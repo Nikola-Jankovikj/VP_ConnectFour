a3550ae9f6a3e2190f6cd7fbaea480394d0b130e
Todor Jovanovski <todor_pp@yahoo.com>
2023-06-26 18:21:50 +0200

    Changed Fonts

--- ConnectFour/MainMenuForm.cs
+++ ConnectFour/MainMenuForm.cs
@@ -25,7 +25,6 @@ namespace ConnectFour
             WithBot = false;
             TurnTimeLimit = 0;
             rbPlayAgainstFriend.Checked = true;
-            ApplyFonts();
         }
 
         private void cbHasTurnTimeLimit_CheckedChanged(object sender, EventArgs e)
@@ -84,13 +83,6 @@ namespace ConnectFour
             }
 
             MessageBox.Show(fileContents, String.Format("How to Play?"), MessageBoxButtons.OK);
-        }
-
-        private void ApplyFonts()
-        {
-            welcomeLbl.Font = new Font("Unispace", 12);
-            btnHelp.Font = new Font("Unispace", 9);
-            startBtn.Font = new Font("Unispace", 10);
         }
     }
 }
